07078f742179a304b11427d9b5ac8762017395b1
Zhang Jiakun <jiakunzhang29@gmail.com>
2026-09-24 09:40:38 +0800

    adjust random input freqency

--- function/genInputFrequency.m
+++ function/genInputFrequency.m
@@ -1,8 +1,13 @@
 function [toneBin, inputFrequency] = genInputFrequency( p )
 
-  %% generate random input frequency
+  %% generate input frequency
 
-  toneBin = datasample( primes( p.fftLen / 2 ), 1 );
+  if p.randomInputFrequency
+    toneBin = datasample( primes( p.fftLen / 2 ), 1 );
+  else
+    toneBin = p.toneBin;
+  end
+
   inputFrequency = p.fs * toneBin / p.fftLen;
 
 end
